fb1a56dbf15610b5450106150cbb9b1bd7a41a66
Uptobillion <w.h@live.cn>
2019-03-21 22:48:44 +0800

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,75 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+//#include <string>
+//#include <cmath>
+
+using namespace std;
+//命名空间 std
+//void swap(int* a, int* b) 
+//{
+//	int temp;
+//	temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//void change(int &a)
+//{
+//	a += 1;
+//}
+int main()
+{
+	/*int x;
+	cin >> x;
+	change(x);
+	cout << x;*/
+	//int a1, b1;
+	//cin >> a1 >> b1;
+	//swap(a1,b1);
+	//cout << a1 << b1 << endl;
+	//int choice;
+	//cin >> choice;
+	//int choice = 2;
+	//switch (choice)
+	//{
+	//case 1:
+	//	cout << "1";
+	//	break;
+	//case 2:
+	//	cout << "2";
+	//	break;
+	//case 3:
+	//	//cout << (double)3;
+	//	printf("%.2f", (double)3);
+	//	break;
+	//default:
+	//	cout << "default";
+	//}
+	//5 > 6 ? 10 : 12;
+	//(int) 3.14f;//强制类型转换
+	//a = cin.get();
+	//std::cin >> a >> b >> c;
+	//std::cout << sizeof(3.14f);//加f表示float
+	/** pre变量 */
+	//string pre = "The volume is: ";
+	//cout << fixed;//用一般的方式输出浮点型，例如Ｃ＋＋程序在控制台显示大一点的数，显示的时候使用了科学计数法，使用该命令即可像一般的方式显示
+	//cout << setprecision(2);//设置精确度为2，并返回上一次的设置。
+	//const float PI = 3.14159;//静态常量
+	//float radius = 4;//Byte
+	//float height = 2;
+	//double volume = PI * pow(radius, 2);//math包
+	//cout << pre << volume << endl;*/
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
