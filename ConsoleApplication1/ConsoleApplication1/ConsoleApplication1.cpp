43967464b29033d5150739d5facfc9b8964be213
Uptobillion <w.h@live.cn>
2019-03-21 22:53:24 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -28,7 +28,7 @@ int main()
 	//int a1, b1;
 	//cin >> a1 >> b1;
 	//swap(a1,b1);
-	//cout << a1 << b1 << endl;
+	cout << a1 << b1 << endl;
 	//int choice;
 	//cin >> choice;
 	//int choice = 2;
@@ -59,17 +59,6 @@ int main()
 	//const float PI = 3.14159;//静态常量
 	//float radius = 4;//Byte
 	//float height = 2;
-	//double volume = PI * pow(radius, 2);//math包
-	//cout << pre << volume << endl;*/
+	double volume = PI * pow(radius, 2);//math包
+	cout << pre << volume << endl;*/
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
